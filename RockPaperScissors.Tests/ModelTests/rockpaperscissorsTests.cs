4989b38c7f7b2d527a81e35c793daa0065bdf751
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-05 15:50:09 -0700

    add initial files Co-authored-by: Cassandra Copp <cass16180@gmail.com> Co-authored-by: Carlos Mendez <carlosmendez86@gmail.com>

---
+++ RockPaperScissors.Tests/ModelTests/rockpaperscissorsTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RockPaperScissors.Models;
+
+namespace RockPaperScissors.Tests
+{
+  [TestClass]
+  public class GameTests
+  {
+   [TestMethod]
+    public void GameConstructor_CreatesInstanceOfGame_Game()
+    {
+      Game newGame = new Game();
+      Assert.AreEqual(typeof(Game), newGame.GetType());
+    }
+    
+  }
+    
+}
